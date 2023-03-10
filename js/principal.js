b859b1a98e501b80ba38dc83a59edb32554ed8e6
Laylla Emily <layllandradef@live.com>
2023-03-10 17:14:51 -0300

    Limitando as casas decimais dos imcs

--- js/principal.js
+++ js/principal.js
@@ -29,6 +29,6 @@ for (var i = 0; i < pacientes.length; i++) {
 
   if (alturaEhValida && pesoEhValido) {
         var imc = peso / (altura * altura);
-        tdImc.textContent = imc;
+        tdImc.textContent = imc.toFixed(2);
     }
 }

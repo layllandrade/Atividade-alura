409c2f565fa622cfbcac90e8b4b017ced26c4bcc
Laylla Emily <layllandradef@live.com>
2023-03-22 00:58:17 -0300

    Exibindo mensagem em caso de erro na requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,20 +1,23 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
-botaoAdicionar.addEventListener("click", function(){
-    console.log("Buscando pacientes...");
-
+botaoAdicionar.addEventListener("click", function() {
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
 
     xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
+        var erroAjax = document.querySelector("#erro-ajax");
 
-        var pacientes = JSON.parse(resposta);
+        if (xhr.status == 200) {
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
     });
 
     xhr.send();
